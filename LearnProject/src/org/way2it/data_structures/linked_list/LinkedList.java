fd087c8fc02232c5ab2bdb21c108eacb2b9206eb
smozolevskyi <mozolevskyi2010@gmail.com>
2022-05-17 11:04:26 +0300

    linked list method add done

--- LearnProject/src/org/way2it/data_structures/linked_list/LinkedList.java
+++ LearnProject/src/org/way2it/data_structures/linked_list/LinkedList.java
@@ -1,5 +1,7 @@
 package org.way2it.data_structures.linked_list;
 
+import java.util.Objects;
+
 public class LinkedList {
 
     // Represents the first node of this list
@@ -14,7 +16,14 @@ public class LinkedList {
 
     // Should add new value to the end of the list and increment length
     public void add(String value) {
-        // TODO implement me
+        if(head == null) {
+            head = new Node();
+            head.value = value;
+            head.next = new Node();
+        } else {
+            addNodeToList(value, head.next);
+        }
+        length++;
     }
 
     // Should add new value next to specified afterValue, increment length and return true
@@ -26,8 +35,11 @@ public class LinkedList {
 
     // Should return true if value exists in this list, false - otherwise
     public boolean contains(String value) {
-        // TODO implement me
-        return false;
+        if (head == null) {
+            return false;
+        } else {
+            return findContainsNodeValue(value, head);
+        }
     }
 
     // Should remove first occurrence of the specified value from this list and decrement the length
@@ -45,4 +57,29 @@ public class LinkedList {
         String value;
         Node next;
     }
+
+    private static void addNodeToList(String value, Node nodeElement) {
+        if (nodeElement.value == null) {
+            nodeElement.value = value;
+            nodeElement.next = new Node();
+        } else {
+           addNodeToList(value, nodeElement.next);
+        }
+    }
+
+    private static boolean findContainsNodeValue(String value, Node nextNode) {
+        if (nextNode == null) {
+            return false;
+        } else if (Objects.equals(nextNode.value, value)) {
+            return true;
+        } else {
+            return findContainsNodeValue(value, nextNode.next);
+        }
+    }
+
+    private static Node findNode(String value, Node nodeElement) {
+        return null;
+    }
+
+
 }
